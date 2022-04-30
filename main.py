39fa9601a2b4c5759b3626f1a6d83791437e7b46
ShyTach1 <shaikelman@gmail.com>
2022-04-30 23:14:05 +0300

    adding main file

--- main.py
+++ main.py
@@ -6,17 +6,28 @@ def main():
     num_of_tries = 0
     secret_word = choose_word()
     old_letters_guessed = []
+    user_progress = ''
     clean_sceen(num_of_tries, secret_word, old_letters_guessed)
 
-    #get an input from the user
     while True:
-        letter_guessed = input("guess a letter")
-    #check valid input
-        if try_update_letter_guessed(letter_guessed, old_letters_guessed) ==True:
-            update_hidden_word(secret_word, old_letters_guessed)
-    #update old letters guessed
-    #update secret word
-    #check win
+        #get an input from the user
+        while True:
+            letter_guessed = input("guess a letter")
+            clean_sceen(num_of_tries, secret_word, old_letters_guessed)
+        #check valid input
+            if try_update_letter_guessed(letter_guessed, old_letters_guessed) == True:
+                user_progress = update_hidden_word(secret_word, old_letters_guessed)
+                break
+
+        print(user_progress)
+        if check_win(secret_word, old_letters_guessed):
+            win_clean(num_of_tries)
+            break
+
+
+        #update old letters guessed
+        #update secret word
+        #check win
 
 
 if __name__ == "__main__":
